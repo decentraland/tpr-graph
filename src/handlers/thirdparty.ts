f88211046b3c08176da6946156969bf8e6ee76dc
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-04-08 15:25:54 -0300

    chore: Remove V2 to only have 1 TPR version

--- src/handlers/thirdparty.ts
+++ src/handlers/thirdparty.ts
@@ -1,10 +1,8 @@
 import { BigInt, Address, log } from '@graphprotocol/graph-ts'
 import { Curation, Item, Receipt, ThirdParty } from '../entities/schema'
 import {
-  ItemAdded,
   ItemReviewed,
   ItemSlotsConsumed,
-  ItemUpdated,
   ThirdPartyAdded,
   ThirdPartyItemSlotsBought,
   ThirdPartyReviewedWithRoot,
@@ -12,14 +10,12 @@ import {
 } from '../entities/ThirdPartyRegistry/ThirdPartyRegistry'
 import {
   buildCountFromCuration,
-  buildCountFromItem,
   buildCountFromReceipt,
   buildCountFromThirdParty
 } from '../modules/Count'
 import { isURNValid } from '../modules/ThirdParty'
 import { buildItemId, isBlockchainIdValid } from '../modules/Item'
 import { buildMetadata } from '../modules/Metadata'
-import { setItemSearchFields } from '../modules/Metadata/Item'
 import { setThirdPartySearchFields } from '../modules/Metadata/ThirdParty'
 
 export function handleThirdPartyAdded(event: ThirdPartyAdded): void {
@@ -148,76 +144,6 @@ export function handleThirdPartyReviewedWithRoot(
   thirdParty.root = event.params._root.toHexString()
 
   thirdParty.save()
-}
-
-export function handleItemUpdated(event: ItemUpdated): void {
-  let itemId = buildItemId(event.params._thirdPartyId, event.params._itemId)
-  let item = Item.load(itemId)
-  if (item == null) {
-    log.error('An item with a non existent id "{}" tried to be updated', [
-      itemId
-    ])
-    return
-  }
-
-  item.rawMetadata = event.params._metadata
-  item.updatedAt = event.block.timestamp
-
-  let metadata = buildMetadata(item.id, item.rawMetadata)
-  item.metadata = metadata.id
-
-  item = setItemSearchFields(item)
-
-  item.save()
-}
-
-export function handleItemAdded(event: ItemAdded): void {
-  if (!isBlockchainIdValid(event.params._itemId)) {
-    log.error('An item was added in the TPR "{}" with an incorrect id "{}"', [
-      event.params._thirdPartyId,
-      event.params._itemId
-    ])
-    return
-  }
-
-  let thirdParty = ThirdParty.load(event.params._thirdPartyId)
-  if (thirdParty == null) {
-    log.error(
-      'An item with id "{}" was added into a non existent TPR with id "{}"',
-      [event.params._itemId, event.params._thirdPartyId]
-    )
-    return
-  }
-  thirdParty.totalItems = thirdParty.totalItems.plus(BigInt.fromI32(1))
-
-  let itemId = buildItemId(event.params._thirdPartyId, event.params._itemId)
-
-  let item = Item.load(itemId)
-  if (item == null) {
-    item = new Item(itemId)
-  }
-
-  item.blockchainItemId = event.params._itemId
-  item.rawMetadata = event.params._metadata
-  item.thirdParty = event.params._thirdPartyId
-  item.createdAt = event.block.timestamp
-  item.updatedAt = event.block.timestamp
-  item.reviewedAt = event.block.timestamp
-  item.isApproved = false
-
-  // As of today, the URN is the same as the item entity id
-  item.urn = itemId
-
-  let metadata = buildMetadata(item.id, item.rawMetadata)
-  item.metadata = metadata.id
-
-  item = setItemSearchFields(item)
-
-  thirdParty.save()
-  item.save()
-
-  let metric = buildCountFromItem()
-  metric.save()
 }
 
 export function handleItemReviewed(event: ItemReviewed): void {
